4d2800ad9afb40cd754a1c764125a46c1c0c1dff
Amol <amolbudhwant2685@gmail.com>
2026-03-02 14:16:21 +0530

    update resend from display name

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -5,8 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export const sendEmail = async ({ to, subject, html }) => {
   try {
     const { data, error } = await resend.emails.send({
-      from: process.env.EMAIL_FROM || `"LinkMint" <onboarding@resend.dev>`,
-      to,
+from: process.env.EMAIL_FROM || "LinkMint <onboarding@resend.dev>",      to,
       subject,
       html,
     });
